f6d03656cb31facf4889879898be2a45afa528c5
KARTHICK KUMAR <62985899+karthicbe2405@users.noreply.github.com>
2021-03-25 11:25:37 +0530

    Update bst.java

--- bst.java
+++ bst.java
@@ -1,38 +1,34 @@
-package bst;
 import java.util.*;
 class Node{
     int value;
     Node left = null;
     Node right = null;
 }
-public class bst {
+public class Main {
     Node root;
     public Node insert(Node temp,int value){
                 if(temp == null){
                     temp = new Node();
                     temp.value = value;
-                    if(root == null)
-                        root = temp;
                     return temp;
                 }
+                //System.out.println(temp.value);
                 if(value < root.value)
                     temp.left = insert(temp.left,value);
-                
-                if(value > root.value)
+                else if(value > root.value)
                     temp.right = insert(temp.right,value);
-
-                //return root;
-    }
+                return temp;
+}
     public void inOrder(Node temp){
             if(temp != null){
                 System.out.println(temp.value);
                 inOrder(temp.left);
                 inOrder(temp.right);
-            }        
+            }
     }
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-		bst o1 = new bst();
+		Main o1 = new Main();
 		System.out.println("Enter the number of values to be Inserted : ");
 		int n = scan.nextInt();
 		System.out.println("Enter the values : ");
